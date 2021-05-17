c60aa340c919977b14664312be37f62a31818a58
sibiraj-s <dev.sibiraj@outlook.com>
2021-05-17 12:35:38 +0530

    chore: update demo

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injector, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Injector, ViewEncapsulation } from '@angular/core';
 import { Editor } from '@tiptap/core';
 import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
@@ -12,7 +12,9 @@ import { AngularComponent, AngularEditableComponent } from './extensions';
   encapsulation: ViewEncapsulation.None
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
+  constructor(private injector: Injector) { }
+
   value = '<p>Hello, Tiptap!</p>'
 
   editor = new Editor({
@@ -48,32 +50,27 @@ export class AppComponent implements OnInit {
     }
   })
 
-  editorA: Editor
-
-  constructor(private injector: Injector) { }
-
-  ngOnInit(): void {
-    this.editorA = new Editor({
-      content: `
-        <p>This is still the text editor you’re used to, but enriched with node views.</p>
-        <angular-component-counter count="0"></angular-component-counter>
-        <p>The below is another counter component with different scope, The count is preset to "1"</p>
-        <angular-component-counter count="1"></angular-component-counter>
-        <p>You can also create an editable component item inside the component</p>
-        <angular-component-editable><p>This is editable</p></angular-component-editable>
-        <p>Did you see that? These are Angular components. We are really living in the future.</p>
-      `,
-      extensions: [
-        StarterKit,
-        Placeholder,
-        AngularComponent(this.injector),
-        AngularEditableComponent(this.injector)
-      ],
-      editorProps: {
-        attributes: {
-          class: 'p-2 border-black focus:border-blue-700 border-2 rounded-md outline-none'
-        }
+  editorA = new Editor({
+    editable: false,
+    content: `
+      <p>This is still the text editor you’re used to, but enriched with node views.</p>
+      <angular-component-counter count="0"></angular-component-counter>
+      <p>The below is another counter component with different scope, The count is preset to "1"</p>
+      <angular-component-counter count="1"></angular-component-counter>
+      <p>You can also create an editable component item inside the component</p>
+      <angular-component-editable><p>This is editable</p></angular-component-editable>
+      <p>Did you see that? These are Angular components. We are really living in the future.</p>
+    `,
+    extensions: [
+      StarterKit,
+      Placeholder,
+      AngularComponent(this.injector),
+      AngularEditableComponent(this.injector)
+    ],
+    editorProps: {
+      attributes: {
+        class: 'p-2 border-black focus:border-blue-700 border-2 rounded-md outline-none'
       }
-    });
-  }
+    }
+  });
 }
