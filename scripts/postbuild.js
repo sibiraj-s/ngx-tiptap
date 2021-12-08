3c70a2ae50b17a7eb443d30ff131a4a8d3362219
sibiraj-s <dev.sibiraj@outlook.com>
2021-12-08 20:15:00 +0530

    chore: prefer node import protocol

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
+const fs = require('node:fs');
+const path = require('node:path');
 const chalk = require('chalk');
 
 const copyFile = async function (srcFilePath, destFilePath) {
