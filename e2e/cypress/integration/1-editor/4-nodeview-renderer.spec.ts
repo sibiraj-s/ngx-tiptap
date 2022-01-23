a83f299a734d3f4633f60c68dd7201142e43e0aa
Sibiraj <dev.sibiraj@outlook.com>
2022-01-23 23:09:07 +0530

    test: add e2e tests for nodeview-renderer

---
+++ e2e/cypress/integration/1-editor/4-nodeview-renderer.spec.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('ngx-tiptap: nodeview-renderer', () => {
+  beforeEach(() => {
+    cy.visit('/nodeview-renderer');
+  });
+
+  it('should render the editor', () => {
+    cy.get('.ProseMirror').should('have.length', 1);
+    cy.get('.ProseMirror').should('have.attr', 'contenteditable');
+    cy.get('.ProseMirror').should('contain.text', 'node views.');
+  });
+
+  it('should render the counter component', () => {
+    cy.get('button').contains('clicked 0 times').click()
+    cy.get('button').contains('clicked 1 times').click()
+    cy.get('button').should('contain.text', 'clicked 2 times')
+  });
+
+  it('should render the editable component', () => {
+    cy.get('.angular-component-with-content').contains('is editable').type('.')
+    cy.get('.angular-component-with-content').should('contain.text', 'This text is editable.')
+  });
+});
