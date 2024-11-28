67dfb564072bac79aaf836d35b28cd07bbd5504e
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2024-11-28 11:45:55 +0530

    chore: fix rendering client components in SSR

--- projects/demo/src/app/app.routes.server.ts
+++ projects/demo/src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
